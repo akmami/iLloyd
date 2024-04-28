65ed7b4c733972b8c4f5beb2134cc5303c85e33b
Akmuhammet Ashyralyyev <akmuhammet99@gmail.com>
2024-04-28 17:11:09 +0300

    header and footer integrated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
 import Canvas from './modules/canvas';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <div className="canvas-container">
         <Canvas />
       </div>
+      <Footer />
     </div>
   );
 }
